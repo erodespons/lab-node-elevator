78e274ffd1987efd51feb51314db3e7273eb098e
Eduardo Rodés <eduardorodes@MacBook-Air-de-Eduardo.local>
2017-11-06 19:10:56 +0100

    Still have to figure out how to make the elevator automatic

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,77 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction = "up";
+    this.passengers = [];
+    this.waitingList = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    setInterval(() => {
+      this.update();
+      this.stop();
+    }, 1000);
+  }
+  stop() {
+    setInterval(() => {
+      if (this.floor > this.requests[this.requests.length-1]) {
+        this.floorDown();
+      } else {
+        this.floorUp();
+      };
+      this.requests.splice();
+    }, 1000);
+  }
+  update() {
+    this.log();
+  }
+  _passengersEnter(person) {
+    this.requests.push(this.waitingList[length-1].destinationFloor);
+    this.passengers.push(this.waitingList[length-1]);
+    console.log(`${person} enters`);
+  }
+  _passengersLeave(person) {
+    console.log(`${person} goes down`);
+  }
+  floorUp() {
+    if (this.floor<10) {
+    this.floor++;
+    this.waitingList.forEach((e,i) => {  //Check if there are passengers waiting to get on the elevator and get them on board.
+      if (e.originFloor == this.floor) {
+        this._passengersEnter(e);
+        this.waitingList.splice(i,1);
+      };
+    });
+    this.passengers.forEach((e,i) => {  //Check if there are passengers waiting to go down and make sure they do.
+      if (e.destinationFloor == this.floor) {
+        this._passengersLeave(e);
+        this.passengers.splice(i,1);
+      };
+    });
+    } else {
+    console.log("Rooftop is for VIPs only.");
+    }
+  }
+  floorDown() {
+    if (this.floor>0) {
+      this.floor--;
+      this.waitingList.forEach((e,i) => {  //Check if there are passengers waiting to get on the elevator and get them on board.
+        if (e.originFloor == this.floor) {
+          this._passengersEnter(e);
+          this.waitingList.splice(i,1);
+        }
+      });
+    } else {
+      console.log("You can't go underground.");
+    }
+  }
+  call(person) {
+    this.waitingList.push(person);
+    this.requests.push(this.waitingList[this.waitingList.length-1].originFloor);
+  }
+  log() {
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,10 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+const elevator = new Elevator();
+
+elevator.start();
+
+let pepe = new Person ("Pepe", 0, 3);
+
+elevator.call(pepe);
